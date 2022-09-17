27652d5943731af01d70b07fb1fb4f6382c92761
slPedr0 <pedro22lima@usp.br>
2022-09-17 14:40:59 -0300

    Included fibonacci using for loop

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 
+
+// Função recursiva
 int fib(int n) {
     if (n==0 || n==1) return 1;
     else return fib(n-1) + fib(n-2);
 }
 
+// Função por laço for
+int fib2(int n) {
+    int soma = 1, aux1 =1, aux2 = 0;
+    for(int i = 0; i < n; i++) {
+        soma = aux1 + aux2;
+        aux2 = aux1;
+        aux1 = soma;
+    }
+    return soma;
+}
+
 int main() {
     int result, n = 10;
-    
-    printf("\nSequência fibonacci até o elemento %d:\n", n);
+
+    // Fibonacci recursivo 
+    printf("\nSequência fibonacci recursiva até o elemento %d:\n", n);
     for(int i = 0; i < n; i++) {
         result = fib(i);
+        printf("|%d| ", result);
+    }
+    printf("\n\n");
+
+    // Fibonacci por laço for
+    printf("\nSequência fibonacci por laço for até o elemento %d:\n", n);
+    for(int i = 0; i < n; i++) {
+        result = fib2(i);
         printf("|%d| ", result);
     }
     printf("\n\n");
